6b218621119ec8e41816dd33d94ff3fa2b24c360
Robohad <ohad@robo-plan.com>
2017-04-25 10:55:16 +0300

    Update Firmware.ino

--- Arduino/Firmware.ino
+++ Arduino/Firmware.ino
@@ -1,16 +1,11 @@
 // Include Libraries
 #include "Arduino.h"
-#include "MPU6050.h"
-#include "Wire.h"
-#include "I2Cdev.h"
 
 // Pin Definitions
 
 // Global variables and defines
-int16_t mpu6050Ax, mpu6050Ay, mpu6050Az;
-int16_t mpu6050Gx, mpu6050Gy, mpu6050Gz;
+
 // Constructors
-MPU6050 mpu6050;
 
 /* This code sets up the essentials for your circuit to work. It runs first every time your circuit is powered with electricity. */
 void setup() {
@@ -20,26 +15,12 @@ void setup() {
     while (!Serial) ; // wait for serial port to connect. Needed for native USB
     Serial.println("start");
     
-    Wire.begin();
-    mpu6050.initialize();
+    //WaterFlow - setup
+    
 }
 
 /* This code is the main logic of your circuit. It defines the interaction between the components you selected. After setup, it runs over and over again, in an eternal loop. */
 void loop() {
-
-    //MPU6050 - Test Code
-    mpu6050.getMotion6(&mpu6050Ax, &mpu6050Ay, &mpu6050Az, &mpu6050Gx, &mpu6050Gy, &mpu6050Gz);   //read accelerometer and gyroscope raw data in three axes
-    double mpu6050Temp = ((double)mpu6050.getTemperature() + 12412.0) / 340.0;
-    Serial.print("a/g-\t");
-    Serial.print(mpu6050Ax); Serial.print("\t");
-    Serial.print(mpu6050Ay); Serial.print("\t");
-    Serial.print(mpu6050Az); Serial.print("\t");
-    Serial.print(mpu6050Gx); Serial.print("\t");
-    Serial.print(mpu6050Gy); Serial.print("\t");
-    Serial.print(mpu6050Gz); Serial.print("\t");
-    Serial.print(F("Temp- "));   
-    Serial.println(mpu6050Temp);
-    delay(100);
-
+    //WaterFlow - Test Code
     
 }
