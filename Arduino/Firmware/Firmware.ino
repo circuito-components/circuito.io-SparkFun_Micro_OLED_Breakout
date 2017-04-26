8608eea4a7b212b913287bb281adafc6a7ee8f99
Ohad Rusnak <ohad@robo-plan.com>
2017-04-26 16:23:19 +0000

    Update repository

--- Arduino/Firmware/Firmware.ino
+++ Arduino/Firmware/Firmware.ino
@@ -1,36 +1,57 @@
-// // ***************************** Include Libraries *************************************
+// This code is processed by the circuito engine, only edit lines within specified code blocks, other changes will be discarded
+// {CIRCUITO_DRIVER_API_1.0}
+
 #include "Arduino.h"
-// ****************************************************************************************
+// ======================= ENTER INCLUDE STATEMETNS HERE // ====================
+// {INCLUDE}
 
-// Pin Definitions
-#define MICROOLED_PIN_CS	4
-#define MICROOLED_PIN_RESET	3
-#define MICROOLED_PIN_DC	2
+// {/INCLUDE}
+// ======================= END OF INCLUDE BLOCK // =============================
 
-// ***************************** Global variables and defines *****************************
+// =======================Pin Definitions - ONLY MODIFY PIN NUMBERS // =========
 
-// ****************************************************************************************
-// ******************************** object initialization *********************************
 
-// ****************************************************************************************
+// =======================Pin Definitions // ===================================
+
+
+// ====================== DEFINE GLOBAL VARIABLES ===============================
+// {GLOBAL}
+
+// {/GLOBAL}
+// ======================= END OF GLOBAL BLOCK // ==============================
+
+
+// // ======================  OBJECT INITIALIZATION // // ====================== 
+// {OBJ}
+
+// {/OBJ}
+// =========================  END OF OBJECT INITIALIZATION // ================== 
+
+
+
 
 /* This code sets up the essentials for your circuit to work. It runs first every time your circuit is powered with electricity. */
-void setup() {
-    // Setup Serial which is useful for debugging
-    // Use the Serial Monitor to view printed messages
-    Serial.begin(9600);
-    while (!Serial) ; // wait for serial port to connect. Needed for native USB
-    Serial.println("start");
+void setup() {                                                                      
+    // Setup Serial which is useful for debugging                                   
+    // Use the Serial Monitor to view printed messages                              
+    Serial.begin(9600);                                                             
+    while (!Serial) ; // wait for serial port to connect. Needed for native USB     
+    Serial.println("start");                                                        
     
-    // ******************************* MicroOLED - setup *****************************
-
-    // *************************************************************************************
+// =========================  SETUP // ========================================= 
+// {SETUP}
+   
+// {/SETUP}
+// =========================  END OF SETUP // ================================== 
 }
 
 /* This code is the main logic of your circuit. It defines the interaction between the components you selected. After setup, it runs over and over again, in an eternal loop. */
 void loop() {
-    // ******************************* MicroOLED - Test Code *************************
-   
-    // *************************************************************************************
+
+// =========================  TESTCODE // ====================================== 
+// {TESTCODE}
+    
+// {/TESTCODE}
+// =========================  END OF TESTCODE // =============================== 
     
 }
