4850b8b9cdd17f8eee9541990c13cbe255a67dd7
Ohad Rusnak <ohad@robo-plan.com>
2017-04-26 12:28:52 +0000

    firmware update

--- Arduino/Firmware.ino
+++ Arduino/Firmware.ino
@@ -1,11 +1,18 @@
-// Include Libraries
+// // ***************************** Include Libraries *************************************
 #include "Arduino.h"
+// ****************************************************************************************
 
 // Pin Definitions
+#define MICROOLED_PIN_CS	4
+#define MICROOLED_PIN_RESET	3
+#define MICROOLED_PIN_DC	2
 
-// Global variables and defines
+// ***************************** Global variables and defines *****************************
 
-// Constructors
+// ****************************************************************************************
+// ******************************** object initialization *********************************
+
+// ****************************************************************************************
 
 /* This code sets up the essentials for your circuit to work. It runs first every time your circuit is powered with electricity. */
 void setup() {
@@ -15,12 +22,15 @@ void setup() {
     while (!Serial) ; // wait for serial port to connect. Needed for native USB
     Serial.println("start");
     
-    //WaterFlow - setup
-    
+    // ******************************* MicroOLED - setup *****************************
+
+    // *************************************************************************************
 }
 
 /* This code is the main logic of your circuit. It defines the interaction between the components you selected. After setup, it runs over and over again, in an eternal loop. */
 void loop() {
-    //WaterFlow - Test Code
+    // ******************************* MicroOLED - Test Code *************************
+   
+    // *************************************************************************************
     
 }
